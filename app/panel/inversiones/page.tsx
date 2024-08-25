04080d46b94eab9eec42bb51931113cb9f84b7f3
nickode <nicopixelsoftware@gmail.com>
2024-08-25 19:06:37 +0200

    feat: design for list inversiones

--- app/panel/inversiones/page.tsx
+++ app/panel/inversiones/page.tsx
@@ -22,11 +22,9 @@ export default async function HipotecasPage({
   const query = searchParams?.query || '';
 
   return (
-    <div className="w-full">
-      <div className="flex w-full items-center justify-between">
-        <h1 className={`${lusitana.className} text-2xl`}>Inversiones</h1>
-      </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+    <div className="">
+      <h1 className="text-2xl pb-5">Inversiones</h1>
+      <div className="mb-8 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Buscar inversiones..." />
         <Link
           href="/panel/inversiones/crear"
--- app/ui/inversiones/list.jsx
+++ app/ui/inversiones/list.jsx
@@ -4,7 +4,7 @@ import { TrashIcon, PencilIcon } from '@heroicons/react/24/outline';
 import { deleteInversionById } from '@/app/lib/actions/inversion';
 import Link from 'next/link';
 
-const CardInversion = ({ inversion }) => {
+const CardInversion2 = ({ inversion }) => {
   const deleteById = deleteInversionById.bind(null, inversion.id);
 
   return (
@@ -148,11 +148,81 @@ const CardInversion = ({ inversion }) => {
   );
 };
 
+const MetricInfo = ({ label, textValue }) => <>
+  <div className="w-full sm:w-1/2 mb-4">
+    <dt className="text-base font-bold leading-7 text-gray-800">{label}</dt>
+    <dd className="text-lg tracking-tight text-gray-800">
+      {textValue}
+    </dd>
+  </div>
+</>
+
+const CardInversion = ({ inversion }) => {
+  return <div className="border-2 border-blue-300 p-10 rounded-md">
+    <h2 className="text-2xl font-bold tracking-tight text-gray-800">{inversion.nombre}</h2>
+    <p className="mt-1 text-gray-800">{new Intl.NumberFormat('es-ES').format(inversion.valor)} €</p>
+    <h2 className="mt-4 mb-3 text-2xl font-bold tracking-tight text-white">
+      {new Intl.NumberFormat('es-ES').format(inversion.alquiler_renta_mes)} € / mes
+    </h2>
+    <hr />
+    <div className="mt-4 flex flex-wrap">
+        <MetricInfo
+          label="ITP"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.itp)} %`}
+        />
+        <MetricInfo
+          label="IBI"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.ibi)} €`}
+        />
+        <MetricInfo
+          label="Notaría"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.notaria)} €`}
+        />
+        <MetricInfo
+          label="Registro"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.registro)} €`}
+        />
+        <MetricInfo
+          label="Gestoría"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.gestoria)} €`}
+        />
+        <MetricInfo
+          label="Reforma"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.reforma)} €`}
+        />
+        <MetricInfo
+          label="Comisión agencia"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.comision_agencia)} €`}
+        />
+        <MetricInfo
+          label="Gastos comunidad"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.comision_agencia)} € / mes`}
+        />
+        <MetricInfo
+          label="Gastos mantenimiento"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.gastos_comunidad)} € / mes`}
+        />
+        <MetricInfo
+          label="Seguro hogar"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.seguro_hogar)} € / año`}
+        />
+        <MetricInfo
+          label="Seguro vida"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.seguro_vida)} € / año`}
+        />
+        <MetricInfo
+          label="Seguro impago"
+          textValue={`${new Intl.NumberFormat('es-ES').format(inversion.seguro_impago)} € / año`}
+        />
+    </div>
+  </div>
+}
+
 export default async function InversionesList({ query }) {
   const inversiones = await fetchFilteredInversiones(query);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-3 gap-4">
+    <div className="flex flex-col lg:flex-row gap-5">
       {inversiones.map((inversion) => <CardInversion inversion={inversion} key={inversion.id} />)}
     </div>
   );
